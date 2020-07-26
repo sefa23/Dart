69aa0ff4bedebba5a4a7e83a12a431282538b39b
sefa23 <65918380+sefa23@users.noreply.github.com>
2020-07-26 22:16:35 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,137 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+void main(){
+
+ runApp(new MaterialApp(
+   theme: ThemeData(
+       primaryColor: Colors.deepOrange,
+       accentColor: Colors.deepOrange,
+   ),
+   debugShowCheckedModeBanner:false ,
+   home: new Scaffold(
+          backgroundColor: Colors.white,
+       appBar: AppBar(
+          title: Text('TASK MANAGMENT',style: TextStyle(fontSize: 18.0),
+       ),
+     ),
+       floatingActionButton: FloatingActionButton(onPressed:(){
+           debugPrint("Tıklandı");
+           debugPrint("Sefa");
+     },
+         child: Icon(Icons.control_point,
+         color: Colors.white,
+     ),
+   ),
+     floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body:Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+
+        children: <Widget>[
+
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+            children: <Widget>[
+             Container(
+               padding: EdgeInsets.all(20.0),
+               margin:EdgeInsets.all (2.0) ,
+               color: Colors.orange.shade50,
+               child: Text("D",
+                 style: TextStyle(fontSize: 25.0),
+               ),
+             ),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                margin:EdgeInsets.all (2.0) ,
+                color: Colors.orange.shade100,
+                child: Text("A",
+                  style: TextStyle(fontSize: 25.0),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                margin:EdgeInsets.all (2.0) ,
+                color: Colors.orange.shade200,
+                child: Text("R",
+                  style: TextStyle(fontSize: 25.0),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                margin:EdgeInsets.all (2.0) ,
+                color: Colors.orange.shade300,
+                child: Text("T",
+                  style: TextStyle(fontSize: 25.0),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            margin:EdgeInsets.all (2.0) ,
+            color: Colors.orange.shade50,
+            child: Text("E",
+              style: TextStyle(fontSize: 25.0),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            margin:EdgeInsets.all (2.0) ,
+            color: Colors.orange.shade100,
+            child: Text("R",
+              style: TextStyle(fontSize: 25.0),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            margin:EdgeInsets.all (2.0) ,
+            color: Colors.orange.shade200,
+            child: Text("S",
+              style: TextStyle(fontSize: 25.0),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            margin:EdgeInsets.all (2.0) ,
+            color: Colors.orange.shade300,
+            child: Text("L",
+              style: TextStyle(fontSize: 25.0),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            margin:EdgeInsets.all (2.0) ,
+            color: Colors.orange.shade400,
+            child: Text("E",
+              style: TextStyle(fontSize: 25.0),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            margin:EdgeInsets.all (2.0) ,
+            color: Colors.orange.shade500,
+            child: Text("R",
+              style: TextStyle(fontSize: 25.0),
+            ),
+          ),Container(
+            padding: EdgeInsets.fromLTRB(24, 20, 24, 20),
+            margin:EdgeInsets.all (2.0) ,
+            color: Colors.orange.shade600,
+            child: Text("İ",
+              style: TextStyle(fontSize: 25.0),
+            ),
+          ),
+        ],
+      )
+
+      ),
+   ),
+ );
+
+}
